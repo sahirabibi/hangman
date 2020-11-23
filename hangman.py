65f6bf6b39789f50bdc47c9a8e7d590531d43c5d
Sahira Bibi <sahirabibi96@gmail.com>
2020-11-23 15:36:01 -0500

    Updated select_word function.

--- hangman.py
+++ hangman.py
@@ -3,3 +3,9 @@
 word_list = ['cat', 'bat', 'hat', 'display', 'network', 'rotate', 'dictionary']
 
 
+def select_word(word_list):
+    print('Selecting word...')
+    return random.choice(word_list)
+
+
+print(select_word(word_box)) 
