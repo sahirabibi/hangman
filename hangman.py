671907ce877bcd2db2adfba5eecd4c6f004e3b29
Sahira Bibi <70290221+sahirabibi@users.noreply.github.com>
2020-12-01 19:54:08 -0500

    Updated hangman, fixed endless game loop
    Updated the game so that after the correct word is guess it ends the game rather than continuing in a loop.

--- hangman.py
+++ hangman.py
@@ -1,66 +1,92 @@
-# hangman game 
-import random 
+# hangman game
+import random
 import collections
 
-words = ['code', 'display','network', 'rotate', 
-            'dictionary','python']
+words = ['code', 'display', 'network', 'rotate',
+         'dictionary', 'python']
+hangman_body = ['head', 'torso', 'right arm',
+                'left arm', 'right leg', 'left leg']
 
 
-def hangman(word_list):   
-    hangman_body = ['head', 'torso', 'right arm', 'left arm', 'right leg', 'left leg'] 
-    print('Selecting word...')
-    w = random.choice(word_list)
-    word = list(w) 
-    results = collections.Counter(word)   
-    print('The game has begun! Hidden word has', len(word), 'letters in it.')
-# game play
-    guesses = []
-    correct_needed = len(word)
-    correct_guessed = 0
-    hidden = ['_' for i in range(len(word))]
-    new_list = []
-    while len(hangman_body) != 0:
-        choice = input('Guess a letter...')
-        print("Guessed letters:", guesses)
+print('Selecting word...')
+# select a random word from word and turn into list
+w = random.choice(words)
+word = list(w)
+print('The game has begun! Hidden word has', len(word), 'letters in it.')
+# count how many times each letter is repeated in word
+results = collections.Counter(word)
+# empty list to store guesses made my user
+guesses = []
+# hidden list replaced each letter in the word with '_' to present to user
+hidden = ['_' for i in range(len(word))]
+repeats = []
+
+
+def play_game():
+    # ask for a letter
+    print("Currently guessed letters:", guesses)
+    choice = input('Guess a letter... to quit, press the spacebar')
+    if choice == ' ':
+        end_game()
+    else:
         if choice in guesses:
-            print("You've already guessed", choice, "try again...")
-            continue
-        elif choice in word:
-            correct_guessed += 1   
-            print('Correct', choice, 'is in the hidden word.')
-            print(choice, 'appears', results[choice], 'times.')
-            guesses.append(choice)
-            new_list.append(choice)
-            for i in new_list:
-                while i in word:
-                    ind = word.index(i)
-                    word[ind] = '-'
-                    new_list.append(i)
-                    hidden[ind] = i
-            print(' '.join(hidden))
-            if correct_guessed == correct_needed:
-                print('You guessed the word!')
-                print(' '.join(hidden))
-                break
+            repeat_guess(choice)
+        else:
+            if choice not in word:
+                wrong_guess(choice)
             else:
-                continue
-        elif choice not in word:
-            if choice in guesses:
-                print('You already guessed that...')
-            else:
-                print('False', choice, 'is not in the hidden word.')      
-                print('Drawing', hangman_body[0],'...')
-                hangman_body.remove(hangman_body[0])
-                if len(hangman_body) == 0:
-                    print("Oh no! You've guessed incorrectly too many times! Hangman is complete.")
-                    print("The hidden word was: ", w)
-                    break
-                else:
-                    continue
-        continue
-    print("Thank you for playing!")
+                correct_guess(choice)
 
 
+def correct_guess(choice):
+    print('Correct', choice, 'is in the hidden word.')
+    print(choice, 'appears', results[choice], 'times.')
+    guesses.append(choice)
+    # replace '-' in hidden word with the choice, do this with each time letter appears in word
+    repeats.append(choice)
+    for i in repeats:
+        while i in word:
+            # count every time letter appears in word and replace it
+            ind = word.index(i)
+            word[ind] = '-'
+            repeats.append(i)
+            hidden[ind] = i
+    print(' '.join(hidden))
+    if '_' not in hidden:
+        winner = True
+        end_game(winner)
+    else:
+        play_game()
+
+
+def wrong_guess(choice):
+    guesses.append(choice)
+    print('False', choice, 'is not in the hidden word.')
+    print('Drawing', hangman_body[0], '...')
+    hangman_body.remove(hangman_body[0])
+    if len(hangman_body) == 0:
+        winner = False
+        end_game(winner)
+    else:
+        play_game()
+
+
+def repeat_guess(choice):
+    print(f'You already guessed {choice}...try again.')
+    play_game()
+
+
+def end_game(winner):
+    if winner == False:
+        print("Oh no! You've guessed incorrectly too many times! Hangman is complete.")
+        print("The hidden word was: ", w)
+    elif winner == True:
+        print('You guessed the word! Congrats!')
+    else:
+        print('Thanks for playing!')
+
+
+play_game()
 hangman(words)
 
 
